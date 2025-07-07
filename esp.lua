23a5769a1d44587d9d0f78fc2dc09270cff26b85
adityaywywi <adityamahardikaputra1212@gmail.com>
2025-07-07 22:47:35 +0900

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,22 +1,25 @@
-print("ESP Loaded")
-
 return function()
-    for _, player in pairs(game:GetService("Players"):GetPlayers()) do
-        if player ~= game.Players.LocalPlayer then
-            if not player.Character:FindFirstChild("Head"):FindFirstChild("ESP") then
-                local esp = Instance.new("BillboardGui", player.Character.Head)
-                esp.Name = "ESP"
-                esp.Size = UDim2.new(0, 100, 0, 40)
-                esp.Adornee = player.Character.Head
-                esp.AlwaysOnTop = true
+	print("ESP Activated")
+	for _, player in pairs(game:GetService("Players"):GetPlayers()) do
+		if player ~= game.Players.LocalPlayer then
+			if player.Character and player.Character:FindFirstChild("Head") then
+				if not player.Character.Head:FindFirstChild("ESP") then
+					local esp = Instance.new("BillboardGui", player.Character.Head)
+					esp.Name = "ESP"
+					esp.Size = UDim2.new(0, 100, 0, 40)
+					esp.Adornee = player.Character.Head
+					esp.AlwaysOnTop = true
 
-                local name = Instance.new("TextLabel", esp)
-                name.Size = UDim2.new(1, 0, 1, 0)
-                name.BackgroundTransparency = 1
-                name.Text = player.Name
-                name.TextColor3 = Color3.new(1, 1, 1)
-                name.TextStrokeTransparency = 0
-            end
-        end
-    end
+					local label = Instance.new("TextLabel", esp)
+					label.Size = UDim2.new(1, 0, 1, 0)
+					label.Text = player.Name
+					label.BackgroundTransparency = 1
+					label.TextColor3 = Color3.new(1, 1, 1)
+					label.TextStrokeTransparency = 0.5
+					label.Font = Enum.Font.SourceSansBold
+					label.TextScaled = true
+				end
+			end
+		end
+	end
 end
